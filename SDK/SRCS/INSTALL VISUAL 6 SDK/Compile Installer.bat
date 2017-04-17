1fb357d8cf44af67f4ea9a7344e0d00f71d6d4da
Kochise <david.koch@9online.fr>
2017-04-17 07:17:22 +0200

    As of 080603 (yes, 2008)
    Upload the legacy software

--- SDK/SRCS/INSTALL VISUAL 6 SDK/Compile Installer.bat
+++ SDK/SRCS/INSTALL VISUAL 6 SDK/Compile Installer.bat
@@ -1,1 +1,19 @@
+@echo off
+
+cd "INPUT\6.0_980820\MSDN\OTHER\DOC\MSDN98\98VSa\1036"
+
+if exist "KB.CHM.001" (
+    if not exist "KB.CHM" (
+        copy /b "KB.CHM.001"+"KB.CHM.002"+"KB.CHM.003" "KB.CHM" 1>nul 2>nul
+    )
+
+    del "KB.CHM.001" /q 1>nul 2>nul
+	del "KB.CHM.002" /q 1>nul 2>nul
+	del "KB.CHM.003" /q 1>nul 2>nul
+)
+
+cd "..\..\..\..\..\..\..\.."
+
+@echo on
+
 ".\TOOLS\Inno Setup 5\ISCC.exe" "Visual C++ 6.0.iss"
